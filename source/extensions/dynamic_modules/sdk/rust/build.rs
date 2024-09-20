1d7d0bb361d6dd5130adc9f02a7e33cb5a77fc72
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-09-20 07:47:44 +0000

    test
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 fn main() {
     println!("cargo:rerun-if-changed=abi.h");
     let bindings = bindgen::Builder::default()
-        .header("abi.h")
+        .header("../../abi.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
